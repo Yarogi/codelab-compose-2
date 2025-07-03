7e106f3d6aabdb29db19398cd6ab11a38fa65f37
Yarogi <143076903+Yarogi@users.noreply.github.com>
2025-07-03 10:58:51 +0300

    добили переход по экранам

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -55,7 +55,7 @@ fun RallyApp() {
             topBar = {
                 RallyTabRow(
                     allScreens = rallyTabRowScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { newScreen -> navController.navigate(newScreen.route) },
                     currentScreen = currentScreen
                 )
             }
